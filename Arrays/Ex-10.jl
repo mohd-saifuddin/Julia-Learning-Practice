74c1f23ac9fc74982d9e7887bf9a95a08bfd95fc
mohd-saifuddin <mohd.saifu42@gmail.com>
2021-09-16 00:21:57 +0530

    Convert Tab indentation to Spaces

--- Arrays/Ex-10.jl
+++ Arrays/Ex-10.jl
@@ -1,14 +1,14 @@
 function flattening(arr)
-	rst = Any[]
-	grab(seq) = for ele in seq 
-					if isa(ele, Array) 
-						grab(ele) 
-					else 
-						push!(rst, ele) 
-					end
-				end
-	grab(arr)
-	return rst
+    rst = Any[]
+    grab(seq) = for ele in seq 
+                    if isa(ele, Array) 
+                        grab(ele) 
+                    else 
+                        push!(rst, ele) 
+                    end
+                end
+    grab(arr)
+    return rst
 end
 
 arr = [1, [1], [1, 1, [1]], [[1]], 1]
