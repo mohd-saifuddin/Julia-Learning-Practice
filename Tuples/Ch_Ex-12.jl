5ae72efe961fa4ee1e9b9518661312cca415c2fa
mohd-saifuddin <mohd.saifu42@gmail.com>
2021-09-28 16:05:24 +0530

    Add hasmatch() function

--- Tuples/Ch_Ex-12.jl
+++ Tuples/Ch_Ex-12.jl
@@ -12,3 +12,29 @@ function sumall(args...)
 end
 
 println(sumall(1, 2, 3, 4))
+
+
+function hasmatch(t1, t2)
+    for (x, y) in zip(t1, t2)
+        if x == y
+            return true
+        end
+    end
+    return false
+end
+
+t1 = [1, 2, 3]
+t2 = [4, 5, 3]
+println(hasmatch(t1, t2))
+
+t1 = [1, 2, 3]
+t2 = [1, 5, 6]
+println(hasmatch(t1, t2))
+
+t1 = [1, 2, 3]
+t2 = [4, 2, 6]
+println(hasmatch(t1, t2))
+
+t1 = [1, 2, 3]
+t2 = [4, 5, 6]
+println(hasmatch(t1, t2))
