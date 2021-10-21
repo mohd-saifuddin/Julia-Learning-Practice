da8cc8a27ad30091e14dc60d5f6b3e11b052c239
mohd-saifuddin <mohd.saifu42@gmail.com>
2021-10-21 23:06:21 +0530

    Ex 14.2 solved

--- Files/Ch-14.jl
+++ Files/Ch-14.jl
@@ -13,3 +13,25 @@ dirname = homedir() * "/Desktop/Julia-Learning-Practice/"
 println(dirname)
 walk(dirname)
 
+
+# Ex 14.2
+
+function sed(pstr, rstr, f1, f2)
+    try
+        f1data = read(f1, String)
+        f1data = replace.(f1data, pstr => rstr)
+        f2file = open(f2, "w")
+        write(f2file, f1data)
+        close(f2file)
+    catch exc
+        println("Root cause ⤑ $exc")
+    finally
+        println("Success!")
+    end
+end
+
+pstr = "Sapians"
+rstr = "Humans"
+f1 = "file1.txt"
+f2 = "file2.txt"
+sed(pstr, rstr, f1, f2)
