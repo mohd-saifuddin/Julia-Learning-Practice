58421954caff69a0c8209645aa105f393142a9e4
mohd-saifuddin <mohd.saifu42@gmail.com>
2021-10-28 22:11:59 +0530

    Ex 16.2 solved

--- StructsFunctions/Ch_Ex-16.jl
+++ StructsFunctions/Ch_Ex-16.jl
@@ -24,3 +24,17 @@ end
 
 time = MyTime(11, 59, 9)
 printtime(time)
+
+
+# Ex 16.2
+
+function isafter(t1, t2)
+    return (t1.hour, t1.minute, t1.second) > (t2.hour, t2.minute, t2.second)
+end
+
+t1 = MyTime(12, 0, 0)
+printtime(t1)
+t2 = MyTime(11, 59, 59)
+printtime(t2)
+
+println(isafter(t1, t2))
