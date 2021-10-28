7bd304c4d8fc75cbbae9a8768b79b6ffb35aa8a0
mohd-saifuddin <mohd.saifu42@gmail.com>
2021-10-28 22:00:31 +0530

    Ex 16.1 solved

--- StructsFunctions/Ch_Ex-16.jl
+++ StructsFunctions/Ch_Ex-16.jl
@@ -1,3 +1,6 @@
+using Printf
+
+
 """
 Represents the time of the day.
 
@@ -12,3 +15,12 @@ end
 time = MyTime(11, 59, 30)
 println(time)
 
+
+# Ex 16.1
+
+function printtime(time)
+    @printf "%02d:%02d:%02d\n" time.hour time.minute time.second
+end
+
+time = MyTime(11, 59, 9)
+printtime(time)
