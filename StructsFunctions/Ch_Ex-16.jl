bc7a69b97d09ac79d7a954481a2264029010517f
mohd-saifuddin <mohd.saifu42@gmail.com>
2021-10-29 16:39:25 +0530

    timetoint() and inttotime() added

--- StructsFunctions/Ch_Ex-16.jl
+++ StructsFunctions/Ch_Ex-16.jl
@@ -36,5 +36,23 @@ t1 = MyTime(12, 0, 0)
 printtime(t1)
 t2 = MyTime(11, 59, 59)
 printtime(t2)
-
 println(isafter(t1, t2))
+
+
+function timetoint(time)
+    minutes = (time.hour * 60) + time.minute
+    seconds = (minutes * 60) + time.second
+    return seconds
+end
+
+function inttotime(seconds)
+    (minutes, second) = divrem(seconds, 60)
+    (hour, minute) = divrem(minutes, 60)
+    return MyTime(hour, minute, second)
+end
+
+time = MyTime(11, 59, 234232)
+seconds = timetoint(time)
+time = inttotime(seconds)
+@printf "%02d:%02d:%02d --> %d\n" time.hour time.minute time.second seconds
+@printf "%d --> %02d:%02d:%02d\n" seconds time.hour time.minute time.second
